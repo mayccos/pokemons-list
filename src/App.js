7928b02d9eae7d91d3badb43c8413f111f0715ad
jean-christophe Becker <mothos@hotmail.fr>
2023-11-26 21:57:05 +0100

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        // Step 1 : "Hello, Heroku ! 👋"
+        axios
+            .get('https://blooming-basin-83218-ff39a65835a7.herokuapp.com')
+            .then((res) => console.log(res.data))
+
+        // Step 2 : "Get JWT token 🔓"
+        axios
+            .post(
+                'https://blooming-basin-83218-ff39a65835a7.herokuapp.com/api/login',
+                { username: 'pikachu', password: 'pikachu' },
+                { headers: { 'Content-Type': 'application/json' } },
+            )
+            .then((res) => res.data)
+            .then((res) => {
+                console.log(res)
+                return res.token
+            })
+            .then((token) => fetchPokemonlist(token))
+    }, [])
+
+    // Step 3 : "Get pokemon list 🎉"
+    const fetchPokemonlist = (token) => {
+        return axios
+            .get(
+                'https://blooming-basin-83218-ff39a65835a7.herokuapp.com/api/pokemons',
+                {
+                    headers: { Authorization: `Bearer ${token}` },
+                },
+            )
+            .then((res) => res.data)
+            .then((res) => console.log(res))
+    }
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello, Heroku ! 👋</h1>
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
